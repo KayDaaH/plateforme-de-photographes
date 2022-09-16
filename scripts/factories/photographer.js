59247f7eb5dd3080b586eb15b482f91ebda43ae0
KayDaaH <thomas36.rolland@gmail.com>
2022-09-16 15:12:37 +0200

    ajout des la description des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,29 +1,39 @@
 function photographerFactory(data) {
-  const { name, city, portrait } = data;
+  const { name, city, country, portrait, tagline, price } = data;
 
-  // const picture = `assets/photographers/${portrait}`;
-  const picture = `assets/photographers/account.png`;
+  const picture = `assets/photographers/${portrait}`;
 
-  //   function getUserCardDOM() {
-  //     const article = document.createElement("article");
-  //     const img = document.createElement("img");
-  //     img.setAttribute("src", picture);
-  //     const h2 = document.createElement("h2");
-  //     h2.textContent = name;
-  //     article.appendChild(img);
-  //     article.appendChild(h2);
-  //     return article;
-  //   }
-  //   return { name, picture, getUserCardDOM };
+  // const picture = `assets/photographers/account.png`;
+
+  // function getUserCardDOM() {
+  //   const article = document.createElement("article");
+  //   const img = document.createElement("img");
+  //   img.setAttribute("src", picture);
+  //   const h2 = document.createElement("h2");
+  //   h2.textContent = name;
+  //   article.appendChild(img);
+  //   article.appendChild(h2);
+  //   return article;
+  // }
+  // return { name, picture, getUserCardDOM };
 
   function getUserCardDOM() {
     const article = document.createElement("article");
     const img = document.createElement("img");
     img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
+    const localisation = document.createElement("localisation");
+    const description = document.createElement("description");
+    const cost = document.createElement("cost");
     h2.textContent = name;
+    localisation.textContent = `${country}, ${city}`;
+    description.textContent = tagline;
+    cost.textContent = `${price}€/jour`;
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(localisation);
+    article.appendChild(description);
+    article.appendChild(cost);
     return article;
   }
   return { name, picture, getUserCardDOM };
