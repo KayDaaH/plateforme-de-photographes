582a9b44faf9ee157a2e05565b7bc4de966fddc1
KayDaaH <thomas36.rolland@gmail.com>
2022-09-19 20:46:22 +0200

    Propre

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,11 +2,6 @@ async function getPhotographers() {
   const data = await fetch("../data/photographers.json");
   const photos = await data.json();
   const photographers = await photos.photographers;
-
-  // setTimeout(() => {
-  //   console.log("Log dans hgetPhotographers() avec un delai de 3 secondes ");
-  // }, 3000);
-
   return photographers;
 }
 
@@ -33,49 +28,9 @@ async function init() {
   displayData(photographers);
   document.querySelectorAll(".url").forEach((item) => {
     item.addEventListener("click", (event) => {
-      event.preventDefault();
       console.log(item.href);
-      //handle click
     });
   });
-  // const lienUrl = document.querySelector(".url");
-  // console.log(typeof lienUrl);
-  // console.log(lienUrl);
-  // lienUrl.addEventListener("click", (bubulle) => {
-  //   bubulle.preventDefault();
-  //   console.log("log addeventlistner");
-  //   console.log(lienUrl);
-  // });
 }
 
 init();
-
-// function resolveAfter2Seconds(x) {
-//   console.log("log dans nouvelle promise");
-//   return new Promise(() => {
-//     setTimeout(() => {
-//       console.log("log dans la promise");
-//     }, 2000);
-//   });
-// }
-
-// async function f1() {
-//   const x = await resolveAfter2Seconds(10);
-//   console.log(x); // 10
-// }
-
-// f1();
-
-// function test2() {
-//   setTimeout(() => {
-//     const url = document.getElementById("url");
-//     url.addEventListener(
-//       "click",
-//       () => {
-//         alert("Hello");
-//       },
-//       5000
-//     );
-//   });
-// }
-// test2();
